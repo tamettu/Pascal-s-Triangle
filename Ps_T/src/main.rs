9eeaff223dcfeb09277e9fd81a963ea68975c67b
tamettu <135158281+tamettu@users.noreply.github.com>
2025-09-22 02:50:02 +0000

    wasd

--- Ps_T/src/main.rs
+++ Ps_T/src/main.rs
@@ -1,20 +1,22 @@
 use std::io::{self,Write};
+use std::time::Instant;
 fn idk(row:i32){
-    if row==1{println!("1");}
+    if row == 0 {()}
+    else if row==1{println!("1");}
     else if row==2{println!("1\n1 1");}
     else{
         println!("1\n1 1\n1 2 1");
         
-        let mut number:Vec<i32> = vec!();
-        let mut new_number:Vec<i32> = vec!();
+        let mut number:Vec<u128> = vec!();
+        let mut new_number:Vec<u128> = vec!();
         number.push(2);
-        for i in 2..row-1{
+        for _ in 2..row-1{
             new_number.clear();
-            new_number.push((number[0]+1));
+            new_number.push(number[0]+1);
             for j in 0..(number.len()-1){
-                new_number.push((number[j]+number[j+1]))
+                new_number.push(number[j]+number[j+1]);
             }
-            new_number.push((number[0]+1));
+            new_number.push(number[0]+1);
             number.clear();
             print!("1 ");
             io::stdout().flush().unwrap();
@@ -27,7 +29,34 @@ fn idk(row:i32){
         }
     }
 }
+fn combination(n: u128, r: u128) -> u128 {
+    let mut result = 1_u128;
+    for i in 0..r {
+        result = result * (n - i) / (i + 1);
+    }
+    result
+}
+fn idk2(row:i32){
+    if !row>0{();}
+    else{
+        for i in 0..row{
+            for j in 0..i{
+                print!("{} ",combination(i as u128, j as u128));
+                io::stdout().flush().unwrap();
+            }
+            println!();
+        }
+    }
+}
 fn main() {
-    idk(10);
+    let start = Instant::now();
+    idk(100);
+    let end = start.elapsed();
+    
+    let start = Instant::now();
+    idk2(100);
+    let end = start.elapsed();
 
+    println!("idk {:?}",end);
+    println!("idk2 {:?}",end);
 }
