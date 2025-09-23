bb0e938139d79f69c77ad99dfbe6a80bdbc86c2e
tamettu <135158281+tamettu@users.noreply.github.com>
2025-09-23 14:22:00 +0000

    idk

--- Ps_T/src/main.rs
+++ Ps_T/src/main.rs
@@ -26,6 +26,38 @@ fn idk(row:i32,output:bool){ //the way i think
         }
     }
 }
+fn idk2(row:i32,output:bool){ //the way i think
+    if row == 0 {()}
+    else if row==1 && output {println!("1");}
+    else if row==2 && output {println!("1\n1 1");}
+    else{
+        if output{println!("1\n1 1\n1 2 1");}
+        let mut number:Vec<u128> = Vec::with_capacity(row as usize);
+        let mut new_number:Vec<u128> = Vec::with_capacity(row as usize);
+        number.push(2);
+        for i in 2..row-1{
+            new_number.clear();
+            new_number.push(number[0]+1);
+            for j in 0..(number.len())/2{
+                if number.len()!=1{
+                new_number.push(number[j]+number[j+1]);
+                }
+            }
+            let skip_count = if i%2 == 0 {0} else {1};
+            for item in new_number.clone().iter().rev().skip(skip_count){
+                new_number.push(*item)
+            }
+            number.clear();
+            let mut a_line:String = "1 ".to_string();
+            for item in &new_number{
+                number.push(*item);
+                a_line.push_str(&format!("{} ",item));
+            }
+            a_line.push_str("1");
+            if output{println!("{}",a_line);}
+        }
+    }
+}
 fn combination(n: u128, r: u128) -> u128 {  //ask ai
     let mut result = 1_u128;
     for i in 0..r {
@@ -33,7 +65,7 @@ fn combination(n: u128, r: u128) -> u128 {  //ask ai
     }
     result
 }
-fn idk2(row:i32,output:bool){  //the way i find on internet
+fn idk3(row:i32,output:bool){  //the way i find on internet
     if !row>0{();}
     else{
         for i in 0..row{
@@ -46,15 +78,33 @@ fn idk2(row:i32,output:bool){  //the way i find on internet
     }
 }
 fn main() {
-    let row=10;
+    let run_time = 1;
+    let row = 12;
+    let output = true;
+    println!("\x1B[2J\x1B[H--- [0/3]");
     let start = Instant::now();
-    idk(row,true);
+    for _ in 0..run_time{
+    idk(row,output);
+    }
     let end = start.elapsed();
-    
-    let start2 = Instant::now();
-    idk2(row,true);
-    let end2 = start2.elapsed();
+    //println!("\x1B[2J\x1B[H#-- [1/3]");
 
-    println!("idk {:?}",end);
-    println!("idk2 {:?}",end2);
+    let start2 = Instant::now();
+    for _ in 0..run_time{
+    idk2(row,output);
+    }
+    let end2 = start2.elapsed();
+    //println!("\x1B[2J\x1B[H##- [2/3]");
+
+    let start3 = Instant::now();
+    for _ in 0..run_time{
+    idk3(row,output);
+    }
+    let end3 = start3.elapsed();
+    //println!("\x1B[2J\x1B[H### [3/3]");
+
+
+    println!("idk run 100 times / 100 = {:?}",end/run_time);
+    println!("idk2 run 100 times / 100 = {:?}",end2/run_time);
+    println!("idk3 run 100 times / 100 = {:?}",end3/run_time);
 }
